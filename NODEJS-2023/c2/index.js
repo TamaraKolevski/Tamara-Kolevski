142a79e4bdb4da682b713407f360f4dc3437cc8f
TamaraKolevski <115027060+TamaraKolevski@users.noreply.github.com>
2023-02-15 20:57:03 +0100

    asd

---
+++ NODEJS-2023/c2/index.js
@@ -0,0 +1,48 @@
+// console.log('zdravo svetu');
+// console.log(5!=5);
+// console.log(typeof 25);
+
+// const chovek = {
+//     name: "John",
+//     surname: "Doe"
+// };
+
+
+// console.log(chovek);
+// a = 10;
+// console.log(a);
+// a = 12;
+// console.log(a);
+// const b = 15;
+// console.log(b);
+
+
+let student = {
+    ime: "Pero",
+    prezime: "Nakov",
+    vozrast: 25
+};
+
+console.log(student);
+
+student = {
+    ime: "Pero",
+    
+};
+
+console.log(student);
+
+
+
+
+
+function testFn() {
+    return 'Test fn initiated';
+}
+
+console.log(testFn());
+let test_fn = testFn();
+
+
+
+

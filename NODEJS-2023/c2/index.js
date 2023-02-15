0f29af6d5e70911c5dca936d312e1fafe5819b1b
TamaraKolevski <115027060+TamaraKolevski@users.noreply.github.com>
2023-02-15 22:01:18 +0100

    c2

--- NODEJS-2023/c2/index.js
+++ NODEJS-2023/c2/index.js
@@ -49,4 +49,32 @@ const testFn1 = () => {
 
 testFn1();
 
+const testFn2 = (b, a = 25) => {
+    return a + b
+    console.log(testFn2(50, 20));
+}
 
+const testFn3 = (0 = {}) => {
+    o.ime = "Zivko"
+    console.log(o);
+}
+const s = {}
+    o.ime = "Zivko"
+    console.log(o);
+
+const convert = (type, value) => {
+    switch (type) {
+        case 'c2f':
+            return value * 9/5 + 32;
+         case 'c2f':  
+         return (value - 32) * 5/9;
+    }
+}
+
+
+const ucenik1 = { ime: "Stefan", prezime: "Petreski", ocenka: 5};
+const ucenik2 = { ime: "Marija", prezime: "Stojanovska", ocenka: 3};
+    
+    
+
+console.log(ucenik);
